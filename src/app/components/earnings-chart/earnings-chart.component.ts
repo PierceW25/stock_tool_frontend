153b3a7bb1bd9c9d8ac0f92e4cda04bd4b8e984a
William Pierce Wierschem <wwwierschem@icloud.com>
2023-09-21 08:59:30 -0400

    Outputting first three elements of earnings array after and before first fiscal year end date is found

--- src/app/components/earnings-chart/earnings-chart.component.ts
+++ src/app/components/earnings-chart/earnings-chart.component.ts
@@ -16,18 +16,23 @@ export class EarningsChartComponent implements OnInit {
   ngOnInit(): void {
     this.stockApiService.getStockEarnings(this.stockSymbol).subscribe((data: any) => {
       let fiscalYearEndMonth = new Date(this.fiscalYearEndMonth + ' 1, 2023').getMonth() + 1;
-      let thirdQuarterEndMonth = fiscalYearEndMonth - 3;
-      let secondQuarterEndMonth = fiscalYearEndMonth - 6;
-      let firstQuarterEndMonth = fiscalYearEndMonth - 9;
       let querterlyEarnings: EarningsDataPoint[] = data['quarterlyEarnings'];
       let allQuarterEndDates: String[][] = [];
-      console.log('all end months ' + fiscalYearEndMonth + ' ' + thirdQuarterEndMonth + ' ' + secondQuarterEndMonth + ' ' + firstQuarterEndMonth)
-      console.log('testing ' + new Date(this.fiscalYearEndMonth + ' 1, 2023').getMonth() + 4)
       for (let i = 0; i < querterlyEarnings.length; i++) {
         let date = new Date(querterlyEarnings[i].fiscalDateEnding);
-        console.log(date)
-      }
-      console.log(allQuarterEndDates)
+        if (date.getMonth() + 1 === fiscalYearEndMonth) {
+          console.log(date)
+          console.log('first three after \n')
+          console.log(new Date(querterlyEarnings[i+1].fiscalDateEnding))
+          console.log(new Date(querterlyEarnings[i+2].fiscalDateEnding))
+          console.log(new Date(querterlyEarnings[i+3].fiscalDateEnding))
+          console.log('first three before \n')
+          console.log(new Date(querterlyEarnings[i-1].fiscalDateEnding))
+          console.log(new Date(querterlyEarnings[i-2].fiscalDateEnding))
+          console.log(new Date(querterlyEarnings[i-3].fiscalDateEnding))
+          break;
+        }
+    }
     });
   }
 }
