f25cf92e25899ef869aa3fb0b109fbc19b4eb78d
William Pierce Wierschem <wwwierschem@icloud.com>
2023-10-05 08:58:14 -0400

    Properly formatted income statement data in a concise way before rendering it to the user, now need to style its display

--- src/app/components/financial-statements/financial-statements.component.ts
+++ src/app/components/financial-statements/financial-statements.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { incomeStatement } from 'src/app/interfaces/incomeStatement';
 import { StockApiService } from 'src/app/services/stock-api.service';
+import { formatLargeNumber } from 'src/app/utils/valueManipulation';
 
 @Component({
   selector: 'app-financial-statements',
@@ -26,10 +27,22 @@ export class FinancialStatementsComponent implements OnInit {
     this.stockApi.getIncomeStatement(privateTicker).subscribe(response => {
       let fullResponse: any = response
       let annualReports = fullResponse['annualReports']
-      let incomeStatement: incomeStatement = annualReports[0]
-      incomeStatement.symbol = privateTicker
-      incomeStatement.statementTitle = 'Income Statement'
+      let rawIncomeStatement = annualReports[0]
+      for (const [key, value] of Object.entries(rawIncomeStatement)) {
+        rawIncomeStatement[key] = this.formatFinancialData(value)
+      }
+      let incomeStatement: incomeStatement = rawIncomeStatement
+
       this.incomeStatement = incomeStatement
     })
+  }
+
+  formatFinancialData(data: any): string {
+    if (isNaN(Number(data))) {
+      return data
+    } else {
+      let dataAsNum = Number(data)
+      return formatLargeNumber(dataAsNum).toString()
+    }
   }
 }
