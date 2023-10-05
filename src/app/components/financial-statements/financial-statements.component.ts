9c210c20b499f1e161f5aac188ed8eafe05b8350
William Pierce Wierschem <wwwierschem@icloud.com>
2023-10-05 06:29:07 -0400

    fetched income statement and put it in object that can be rendered

--- src/app/components/financial-statements/financial-statements.component.ts
+++ src/app/components/financial-statements/financial-statements.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { incomeStatement } from 'src/app/interfaces/incomeStatement';
+import { StockApiService } from 'src/app/services/stock-api.service';
 
 @Component({
   selector: 'app-financial-statements',
   templateUrl: './financial-statements.component.html',
   styleUrls: ['./financial-statements.component.css']
 })
-export class FinancialStatementsComponent {
+export class FinancialStatementsComponent implements OnInit {
+  constructor(private stockApi: StockApiService) { }
 
+  @Input() ticker: string = ''
+  @Input() selectedStatement: string = ''
+
+  incomeStatement: any
+
+  ngOnInit(): void {
+    this.incomeStatement = this.createIncomeStatement(this.ticker)
+  }
+
+
+  createIncomeStatement(stockSymbol: string) {
+    let privateTicker = stockSymbol
+
+    this.stockApi.getIncomeStatement(privateTicker).subscribe(response => {
+      let fullResponse: any = response
+      let annualReports = fullResponse['annualReports']
+      let incomeStatement: incomeStatement = annualReports[0]
+      return incomeStatement
+    })
+  }
 }
