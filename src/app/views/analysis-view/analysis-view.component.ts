0f71942c0e5af7d310782121023d16346ff57aba
William Pierce Wierschem <wwwierschem@icloud.com>
2023-10-05 10:15:57 -0400

    Fixed market news article links, the whole div lighted up when hovered but only took the user to the article when the title was clicked, now if the user clicks anywhere in the div the article is gone to

--- src/app/views/analysis-view/analysis-view.component.ts
+++ src/app/views/analysis-view/analysis-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-analysis-view',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AnalysisViewComponent {
 
+
+  @Input() stockSymbol: string = '';
+  searchTerm: string = '';
 }
