4b815dd6e919e0e2b61bbcf5ff7d60830ccd9864
William Pierce Wierschem <wwwierschem@icloud.com>
2023-11-01 09:07:58 -0400

    Finished initial styling of account info page, making it functional now

--- src/app/views/account-view/account-view.component.ts
+++ src/app/views/account-view/account-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-account-view',
@@ -8,9 +8,16 @@ import { Component } from '@angular/core';
 export class AccountViewComponent {
   constructor() {}
 
+  @Input() name: string = '';
+  @Input() email: string = '';
+  @Input() watchlist1Title: string = '';
+  @Input() watchlist2Title: string = '';
+  @Input() watchlist3Title: string = '';
+  @Input() watchlist1: string[] = [];
+  @Input() watchlist2: string[] = [];
+  @Input() watchlist3: string[] = [];
+
   nameInputEnabled = false;
-  emailInputEnabled = false;
-  passwordInputEnabled = false;
   watchlist1InputEnabled = false;
   watchlist2InputEnabled = false;
   watchlist3InputEnabled = false;
